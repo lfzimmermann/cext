f7c0e72b1e719e9c07dfcf1ed9ede50a4679d8e6
Luca Frank Zimmermann <luca-frank.zimmermann@stud.uni-bamberg.de>
2025-06-24 17:52:50 +0200

    Added unit tests via CUnit.

--- tests/gap_buffer_test.c
+++ tests/gap_buffer_test.c
@@ -1,1 +1,51 @@
+#include "gap_buffer_test.h"
+#include "../src/backend/gap_buffer.h"
+#include <stdlib.h>
+#include <string.h>
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+
+void test_create_gap_buffer(void){
+    struct Gap_Buffer_Struct* gp = gp_create();
+    CU_ASSERT_TRUE(gp != NULL);
+    free(gp->buffer);
+    free(gp);
+}
+
+void test_destroy_gap_buffer(void){
+    struct Gap_Buffer_Struct* gp = gp_create();
+    gp_destroy(gp);
+
+}
+
 void test_delete_char(void){
+    struct Gap_Buffer_Struct* gp = gp_create();
+    { // insert mock 'T'
+        gp->buffer[gp->gap.start] = 'T';
+        gp->content_len += 1;
+        gp->gap.start++;
+        gp->gap.size--;
+    }
+    CU_ASSERT_TRUE(gp->content_len == 1);
+    CU_ASSERT_TRUE(strcmp(gp_get_buffer_content(gp), "T:")); // TODO, remove ':' character for visualization
+    gp_delete(gp);
+    CU_ASSERT_TRUE(gp->content_len == 0);
+    CU_ASSERT_TRUE(strcmp(gp_get_buffer_content(gp), ":")); // TODO, remove ':' character for visualization
+    gp_destroy(gp);
+
+}
+
+void test_insert_char(void){
+    struct Gap_Buffer_Struct* gp = gp_create();
+    CU_ASSERT_TRUE(gp->content_len == 0);
+    CU_ASSERT_TRUE(strcmp(gp_get_buffer_content(gp), ":")); // TODO, remove ':' character for visualization
+    gp_insert(gp, 'T');
+    CU_ASSERT_TRUE(gp->content_len == 1);
+    CU_ASSERT_TRUE(strcmp(gp_get_buffer_content(gp), "T:")); // TODO, remove ':' character for visualization
+    gp_destroy(gp);
+
+}
+
+void test_move_cursor(void){
+    return;
+}
