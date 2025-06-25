51f71d91cd08f92efb6807f4a977ec6c015c224f
Luca Frank Zimmermann <luca-frank.zimmermann@stud.uni-bamberg.de>
2025-06-25 15:25:51 +0200

    Removed unnecessary statement.

--- tests/gap_buffer_test.h
+++ tests/gap_buffer_test.h
@@ -6,7 +6,6 @@ void test_insert_char(void);
 void test_create_gap_buffer(void);
 void test_destroy_gap_buffer(void);
 void test_delete_char(void);
-void test_insert_char(void);
 void test_move_cursor(void);
 
 #endif // GAP_BUFFER_TEST_H_
